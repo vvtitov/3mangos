c06d1b445d609d9c3357d0616427548ee79bec28
Vladislav Titov <42722011+vvtitov@users.noreply.github.com>
2025-01-24 04:06:11 -0300

    fix: minor color fixes and display

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="items-center justify-items-center h-full w-full select-none">
-      <BackgroundLines className="grid items-center justify-center w-full h-full">
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="mt-[40vh] bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-4xl md:text-6xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
-            3MANGOS <br /> AGENCY
-          </h1>
-          <Image
-            width={400}
-            height={400}
-            src="/mango.png"
-            alt="Mango image"
-            className="absolute top-32"
-          />
-        </div>
-        <p className="mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center mt-[20vh] md:mt-[5vh]">
-          Esta pagina web se encuentra en construccion. <br />
-          <strong>contacto@3mangos.site</strong>
-        </p>
+      <BackgroundLines className="flex items-center justify-center h-full w-full min-h-auto bg-[#9cd3aa] min-h-screen">
+        <Image
+          width={450}
+          height={450}
+          src="/3m-nobg.png"
+          alt="3mango logo"
+          className="absolute top-32"
+        />
+        <h1 className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-4xl md:text-5xl font-sans z-20 font-bold mt-[45vh]">
+          UNDER CONSTRUCTION <br />
+        </h1>
       </BackgroundLines>
     </div>
   );
