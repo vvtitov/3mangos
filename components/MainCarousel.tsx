94f95ac038edecd656ff21dc81cf0d9c7f477c07
Vladislav Titov <42722011+vvtitov@users.noreply.github.com>
2025-01-28 19:59:47 -0300

    Revert "add: hero, navmenu, minor ui fixes"

---
+++ components/MainCarousel.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Carrousel from "./ui/carousel";
+
+export default function MainCarousel() {
+  return (
+    <div className="flex overflow-hidden w-full h-full mt-[12vh] justify-center z-0 p-10">
+      <Carrousel
+        slides={[
+          {
+            title: "We are 3MANGOS",
+            button: "Click me",
+            src: "/mango.png",
+          },
+          {
+            title: "We are 3MANGOS",
+            button: "Click me",
+            src: "/mango.png",
+          },
+          {
+            title: "We are 3MANGOS",
+            button: "Click me",
+            src: "/mango.png",
+          },
+          {
+            title: "We are 3MANGOS",
+            button: "Click me",
+            src: "/mango.png",
+          },
+          {
+            title: "We are 3MANGOS",
+            button: "Click me",
+            src: "/mango.png",
+          },
+          {
+            title: "We are 3MANGOS",
+            button: "Click me",
+            src: "/mango.png",
+          },
+        ]}
+      />
+    </div>
+  );
+}
